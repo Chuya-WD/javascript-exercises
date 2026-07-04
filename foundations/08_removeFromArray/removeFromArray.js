9e49371f587afe3ae01e7d6f0d465c1eb4bf282a
Chuya <245904786+Chuya-WD@users.noreply.github.com>
2026-07-03 22:33:42 -0500

    Solución Ejercicio 8

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,20 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...numRm) {
+    let result=[];
+    for(let i=0; i<arr.length; i++){
+        let val=false; 
+        for(let j=0; j<numRm.length; j++){
+            if(arr[i]===numRm[j]){
+                val=true; 
+                break; 
+            }
+        }
+        if(val){
+            continue;
+        }else{
+            result.push(arr[i]); 
+        }
+    }
+    return result; 
 };
 
 // Do not edit below this line
