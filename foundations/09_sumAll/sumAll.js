daa7206350294dc2b38559eeeff7de6cfd00970c
Chuya <245904786+Chuya-WD@users.noreply.github.com>
2026-07-05 00:06:26 -0500

    Sumar Todo

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    let min=0, max=0, result=0; 
 
+    if(a === b || a<0 || b<0 || typeof a !== "number"|| typeof b !== "number" || !Number.isInteger(a) || !Number.isInteger(b)){
+        return "ERROR"; 
+    }else if(a>b){
+        max=a; 
+        min=b;
+    }else{
+        max=b; 
+        min=a;
+    }
+
+    for(let i=min; i<=max; i++){
+        result+=i; 
+    }
+    return result; 
 };
 
 // Do not edit below this line
